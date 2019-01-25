3dcc4c971e4b53f622055be7f29417911c100f59
tankhust <32664590+tankhust@users.noreply.github.com>
2019-01-25 14:59:33 +0800

    Add files via upload

---
+++ vueAdmin-template-master-master/config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://127.0.0.1:7300/mock/5b7bfe48019a2a1692614d8f/tensquare"',
+})
